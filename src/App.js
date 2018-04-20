73f444f598a2ea7686eb77d75ce7093f54849e87
Eric Weinberg <egweinberg@viterbo.edu>
2018-04-20 15:57:21 -0500

    working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,142 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import request from 'superagent';
+import logo from './logo-red.svg';
 import './App.css';
+import Slider from "react-slick";
+import Sound from 'react-sound';
+
+
+let fillimg = {
+  width:'90vw',
+  height:'80vh'
+}
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    }
+
+    //http://viterbouniveristyd8dev.prod.acquia-sites.com/
+    this.server_name="http://viterbouniveristyd8dev.prod.acquia-sites.com/";
+
+    this.fetchPhotos = this.fetchPhotos.bind(this)  //needed for reference below
+    this.checkAlert = this.checkAlert.bind(this)  //needed for reference below
+    this.isAlert=false;
+  }
+
+  componentWillMount() {
+    this.fetchPhotos();
+    var intervalId = setInterval((this.fetchPhotos), 180000); // 3 minutes in milliseconds
+    var intervalId2 = setInterval((this.checkAlert),  10000); // 10 seconds
+  }
+
+  fetchPhotos() {
+    request
+        .get(this.server_name + 'm1')
+        .then((res) => {
+      this.setState({
+      photos: res.body
+    })
+  })
+  }
+  checkAlert() {
+    request
+        .get('http://libservices.viterbo.edu/al/alerts/site.php')
+        .then((res) => {
+      var rslts = JSON.parse(res.text)
+      if (rslts.status === true){ ///las
+      console.log('alert');
+      this.isAlert=true;
+      var alert ={title: "alert",body: "testing 123 asdfasdfasdfasdfasdf asddfasdfasfdsa", nid:"001",field_event_image:"attention-clipart.jpg",field_location_name:"Notice"};
+      //var newPlayer = Object.assign({}, player, {score: 2});
+      this.server_name="/";
+      var newArray= [];
+      newArray.push(alert);
+      this.setState({
+        photos: newArray
+      })
+    }
+  else{
+      this.isAlert=false;
+      console.log('no alert');
+
+    }
+  })
+
+  }
+
   render() {
+    const isAlert = this.isAlert;
+    const sound = isAlert ? (
+    <Sound
+        url="/alarm.wav"
+    playStatus={Sound.status.PLAYING}
+    playFromPosition={0 /* in milliseconds */}
+    onLoading={this.handleSongLoading}
+    onPlaying={this.handleSongPlaying}
+    onFinishedPlaying={this.handleSongFinishedPlaying}
+  />
+  ) : (
+    <nosound/>
+  );
+    var speed=11000;
+    if (this.state.photos.length>0){
+      speed=this.state.photos[0].Speed;
+    }
+    var settings = {
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: speed,
+      cssEase: "linear",
+      arrows: false,
+    };
     return (
-      <div className="App">
+
+        <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <div className="outer">
+        {sound}
+        <Slider {...settings}>
+    {this.state.photos.map((photo, key) => {
+      return (
+    <div key={photo.nid} className="yabba">
+        {console.log(photo)}
+    <div className="slide-container">
+    {photo.Type === 'Event' ? (
+    <div>
+    <h1 >{photo.title}</h1>
+    <img alt="banner" className="scroll-img" src= {this.server_name + photo.field_event_image}/>
+    <h4>{photo.field_event_dates}</h4>
+    <h4>{photo.field_location_name}</h4>
+    <p></p>
+    <p>{photo.body}</p>
+    </div>
+
+    ) : (
+    <div>
+    <img alt="banner" className="scroll-img" style={fillimg} src= {this.server_name + photo.field_banner_image}/>
+    </div>
+    )}
+
+
+
+    </div>
+    </div>
+    )
+    })}
+  </Slider>
+    </div>
+    </div>
+  );
   }
 }
 
