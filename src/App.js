7c11241115541e7901828655e0d0b98124b09f0d
Eric Weinberg <egweinberg@viterbo.edu>
2019-06-17 08:08:18 -0500

    social migrations

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Slider from "react-slick";  //slider library loaded via npm
 import Sound from 'react-sound';  //sound library loaded via npm
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import $ from 'jquery';
 
 //centering inline css for slide
 let fillimg = {
@@ -26,13 +27,15 @@ class App extends Component {
     console.log(this.monitor); // ["name"]
     this.fetchPhotos = this.fetchPhotos.bind(this)  //needed for reference below
     this.checkAlert = this.checkAlert.bind(this)  //needed for reference below
+    this.reportStatus = this.reportStatus.bind(this)  //needed for reference below
     this.isAlert=false;
   }
 
   componentDidMount() {
     this.fetchPhotos();
     var intervalId = setInterval((this.fetchPhotos), 180000); // 3 minutes in milliseconds
-    var intervalId2 = setInterval((this.checkAlert),  10000); // 10 seconds
+    var intervalId2 = setInterval((this.checkAlert),  8000); // 8 seconds
+    var intervalId3 = setInterval((this.reportStatus),  35000); // 25 seconds
   }
 
   fetchPhotos() {  //call api from drupal to get slides, stores in photos
@@ -46,31 +49,45 @@ class App extends Component {
     })
   })
   }
+  reportStatus() {  //call api from drupal to get slides, stores in photos
+    var url = new URL(window.location.href);
+    var monitor = url.searchParams.get("monitor");
+    var curr = $('.slick-track').children('.slick-slide').css('opacity','1');
+    request
+        .get('http://libservices.viterbo.edu/al/alerts/reportstatus.php?monitor='+ monitor)
+        .then((res) => {
+          console.log('res');
+        })
+  }
   checkAlert() {  //checks libservices for alert json string to see if active
     request
-        .get('http://libservices.viterbo.edu/al/alerts/site.php')
-        .then((res) => {
-      var rslts = JSON.parse(res.text)
-      if (rslts.status === true){ ///las
-      console.log('alert');
-      this.isAlert=true;
-      var msg = rslts.message;
-      var alert ={title: "alert",body: msg, nid:"001",field_event_image:"attention-clipart.jpg",field_location_name:"Notice"};
-      //var newPlayer = Object.assign({}, player, {score: 2});
-      this.server_name="/";
-      var newArray= [];
-      newArray.push(alert);
-      this.setState({
-        photos: newArray
-      })
-    }
-  else{
-      this.isAlert=false;
-      console.log('no alert');
-
-    }
-  })
-
+          .get('http://libservices.viterbo.edu/al/alerts/site.php')
+          .then((res) => {
+        var rslts = JSON.parse(res.text)
+        if (rslts.status === true){ ///las
+        console.log('alert');
+        this.isAlert=true;
+        var msg = rslts.message;
+        this.server_name="http://libservices.viterbo.edu/al/alerts/";
+        var alert ={title: "alert",body: msg, nid:"001",field_event_image:"attention-clipart.jpg",field_location_name:"Notice",type:"Alert"};
+        //var newPlayer = Object.assign({}, player, {score: 2});
+        var newArray= [];
+        newArray.push(alert);
+        this.setState({
+          photos: newArray
+        })
+      }
+    else{
+        this.isAlert=false;
+        if(this.isAlert==true){
+          this.isAlert=false;
+          console.log('no alert');
+          window.location.reload();
+        }
+          this.isAlert=false;
+          console.log('no alert');
+        }
+    })
   }
 
   render() {
@@ -117,26 +134,32 @@ class App extends Component {
         {sound}
         <Slider {...settings}>
           {this.state.photos.map((photo, key) => { //loop through photos to output each photo inside slider widget
-          return (
-              <div key={photo.nid} className="slide-outer">
-              {console.log(photo)}
-              <div className="slide-container">
-              {photo.type === 'Event' ? ( //differnt content depending on type
-                <div className="inner-slide event">
-                <h1 >{photo.title}</h1>
-                <img className="scroll-img evt-img" src= {this.server_name + photo.field_event_image}/>
-                <h2><i className="fa fa-clock-o" aria-hidden="true"></i>&nbsp;{photo.field_event_date}</h2>
-                <h2><i className="fa fa-location-arrow" aria-hidden="true"></i>&nbsp;{photo.field_location_name}</h2>
-                <p></p>
+            return (
+                <div key={photo.nid} className="slide-outer">
+                  {console.log(photo)}
+                  <div className="slide-container">
+                    {photo.type === 'Event' ? ( //differnt content depending on type
+                        <div className="inner-slide event">
+                          <h1 >{photo.title}</h1>
+                          <img className="scroll-img evt-img" src= {this.server_name + photo.field_event_image}/>
+                          <h2><i className="fa fa-clock-o" aria-hidden="true"></i>&nbsp;{photo.field_event_date}</h2>
+                          <h2><i className="fa fa-location-arrow" aria-hidden="true"></i>&nbsp;{photo.field_location_name}</h2>
+                          <p>{photo.body}</p>
+                        </div>
+                    ):photo.type === 'Scrolling Image' ?(//image type
+                        <div className="inner-slide photo">
+                          <img className="scroll-img" style={fillimg} src= {this.server_name + photo.field_scroller_image}/>
+                        </div>
+                    ): (// alert
+                        <div className="inner-slide event">
+                          <h1 >{photo.title}</h1>
+                          <img className="scroll-img evt-img" src= {this.server_name + photo.field_event_image}/>
+                          <h2>{photo.body}</h2>
+                        </div>
+                    )}
+                  </div>
                 </div>
-              ) : (//image type
-                <div className="inner-slide photo">
-                <img className="scroll-img" style={fillimg} src= {this.server_name + photo.field_scroller_image}/>
-                </div>
-              )}
-              </div>
-              </div>
-              )
+            )
           })}
         </Slider>
       </div>
