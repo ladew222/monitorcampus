d27dca3aaf4c8180d93cfa68bc860aaa6c361f32
ericweinberg <egweinberg@viterbo.edu>
2021-06-30 10:32:45 -0500

    player

--- src/VideoPlayer.js
+++ src/VideoPlayer.js
@@ -1,30 +1,20 @@
 
 import React from "react";
-import Hls from "hls.js";
+import ReactDOM from 'react-dom';
+import ReactHlsPlayer from 'react-hls-player';
 export default class VideoPlayer extends React.Component {
-    state = {};
     componentDidUpdate() {
-    const video = this.player;
-       let config = {
-            maxMaxBufferLength: 1,
-            liveSyncDuration: 0.5,
-            liveMaxLatencyDuration: 1,
-            liveBackBufferLength: 0,
-            nudgeMaxRetry: 10
-        };
-        const hls = new Hls(config);
-        const url = "https://monitors.viterbo.edu/vid/file.m3u8";
 
-        hls.loadSource(url);
-        hls.attachMedia(video);
-        hls.on(Hls.Events.MANIFEST_PARSED, function() { video.play(); });
     }
     render() {
         return (
-            <video
-                className="videoCanvas"
-                ref={player => (this.player = player)}
-                autoPlay={true}
+            <ReactHlsPlayer
+                src="https://monitors.viterbo.edu/vid/file.m3u8"
+                hlsConfig={{
+                    maxLoadingDelay: 4,
+                    minAutoBitrate: 0,
+                    lowLatencyMode: true,
+                }}
             />
         );
     }
